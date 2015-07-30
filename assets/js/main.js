dd8809b34dde7efe6a393317feefa8736deb2b51
Vaneyck <vaneycksemeotoki@gmail.com>
2015-07-30 10:28:23 +0300

    WEB-138 Add customers panel

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var ProductSite = function () {
+	var init = function () {
+		initRecentBlogPostFetcher();
+	};
+
+	var initRecentBlogPostFetcher = function () {
+		var url  = "http://www.frontlinesms.com/?json=get_recent_posts&count=1&callback=?";
+		$.getJSON(url, function(result){
+			var recentPost = result.posts[0];
+			console.log(recentPost.thumbnail_images.medium.url);
+			$('.recent-post-title').html(recentPost.title);
+			$('.recent-post-image').attr('src', recentPost.thumbnail_images.medium.url);
+			$('.recent-post-excerpt').html(recentPost.excerpt);
+		});
+	}
+
+	init();
+}
+
+var productSite;
+$(function(){
+	productSite = new ProductSite();
+});
